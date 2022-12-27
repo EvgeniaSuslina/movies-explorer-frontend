a10ccc5cff2e9555d8c868756ed86a839202892a
Evgenia Suslina <zhenechka-95@yandex.ru>
2022-12-27 20:44:17 +0300

    refactor validaton Search Form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,44 +1,70 @@
 import './SearchForm.css';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import {useState} from "react"
+import { useEffect, useState } from 'react';
+
+const useValidation = (value, validations) => {
+    const [isEmpty, setEmpty] = useState(true)
+
+    useEffect(() => {
+        for (const validation in validations) {
+            switch(validation) {
+                case 'isEmpty' :
+                    value ? setEmpty(false) : setEmpty(true) 
+                break;  
+                default: 
+                break                        
+            }
+        }
+    }, [value, validations])
+
+    return {
+        isEmpty
+    }
+
+}
+
+const useInput = (initialValue, validations) =>{
+    const [value, setValue] = useState(initialValue)
+    const [isDirty, setDirty] = useState(false)
+    const valid = useValidation(value, validations)
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const onBlur = () => {
+        setDirty(true)
+    }
+
+    return{
+        value, 
+        isDirty,
+        onChange,
+        onBlur,
+        ...valid
+    }
+}
 
 function SearchForm(){
-
-    const [isValid, setIsValid] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [inputValue, setInputValue] = useState('');
-
-    function handleInputChange(evt) {
-        setInputValue(evt.target.value);
-        setIsError(false);
-        setIsValid(evt.target.validity.valid);
-    }
-
-    function handleSubmitForm(evt) {
-        evt.preventDefault();
-
-        if (!isValid || inputValue.length === 0) {
-            setIsError(true);
-        } else {
-            setIsError(false);
-        }
-    }
-
+    const filmName = useInput('', {isEmpty: true})
 
     return (
-        <div className="search-form-content">
-            <form className="search-form" noValidate onSubmit={handleSubmitForm}>
+        <div className="search-form-content">            
+            <form className="search-form" noValidate>                
                 <input 
                 className="search-form__input" 
                 placeholder="Фильм"
+                name="filmName"
                 required
                 type="text"
                 minLength="1"
-                onChange={handleInputChange}
+                value={filmName.value}
+                onChange={e => filmName.onChange(e)}
+                onBlur={e => filmName.onBlur(e)}
                 />
-            <button className="search-form__button" type="submit"></button>
-            <span className="error-message">{isError && 'Нужно ввести ключевое слово'}</span>
+            <button className="search-form__button" type="submit"></button>            
             </form>
+            {(filmName.isDirty && filmName.isEmpty) && <span className="search-form__error">Нужно ввести ключевое слово</span>}
             <FilterCheckbox />
         </div>
     )
